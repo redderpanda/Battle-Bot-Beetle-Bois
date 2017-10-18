103bd0802b74664cf5c58d70cbb6a501a9c054f9
Jared Chun <jaredsc@uci.edu>
2017-10-17 22:23:30 -0700

    Add Unity Project

---
+++ Battle-Bots Beetle Bois/Assets/Scripts/Microwave.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Microwave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //public void MicrowaveClick()
+        //{
+        //Debug.Log(ulong)DateTime.Now.Ticks;
+        //}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
