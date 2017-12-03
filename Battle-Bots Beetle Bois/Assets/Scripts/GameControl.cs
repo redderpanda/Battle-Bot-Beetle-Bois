a13e4817b248d686f6d41b0211f1af72de8614b7
Nikhil Sharma <nik41507@gmail.com>
2017-12-03 15:24:34 -0800

    Game Stats part 1
    Game Stats are now being displated. Plus a few other things, such as the text for the missions

--- Battle-Bots Beetle Bois/Assets/Scripts/GameControl.cs
+++ Battle-Bots Beetle Bois/Assets/Scripts/GameControl.cs
@@ -18,10 +18,15 @@ public class GameControl : MonoBehaviour
 
     public static GameControl currentPlayer;
 
+    public GameObject gameStats;
+    public GameObject currentStats; 
+
+
     public string name;
     public int level;
     public int exp;
-    
+    public int rupees;
+    public int beetleBucks;
     public List<BBBBLegs> Legs = new List<BBBBLegs>();
     public List<BBBBArms> Arms = new List<BBBBArms>();
     public List<BBBBBase> Base = new List<BBBBBase>();
@@ -32,30 +37,44 @@ public class GameControl : MonoBehaviour
     
     private void Awake() //There should be only one playerData. If a playerData is made that is not the static currentPlayer, kill it for its existance is blasphemy 
     {
+        Debug.Log("Life");
         if (currentPlayer == null)
         {
+            Debug.Log("Does this load everytime");
             DontDestroyOnLoad(gameObject);
             currentPlayer = this;
-          
-
         }
         else if (currentPlayer != this)
         {
+            Debug.Log("Does this load everytime");
             Destroy(gameObject);
         }
+
+
     }
 
     // Use this for initialization
     void Start()
     {
         name = "Player name";
-        level = 0;
-        exp = 0;
-
-        Load();
-     
+        level = 2;
+        exp = 500;
+        rupees = 1000;
+        beetleBucks = 1000;
+        //Load();
+        displayStats();
        
         
+
+    }
+
+    public void DidReloadScripts()
+    {
+        Debug.Log("Looksl ike it has reloarded");
+    }
+
+    public void displayStats()
+    {
 
     }
 
@@ -68,6 +87,8 @@ public class GameControl : MonoBehaviour
         data.name = name;
         data.level = level;
         data.exp = exp;
+        data.rupees = rupees;
+        data.beetleBucks = beetleBucks;
 
         
         
@@ -85,9 +106,11 @@ public class GameControl : MonoBehaviour
             PlayerData data = (PlayerData)bf.Deserialize(file);
             file.Close();
 
-     level = data.level;
+        level = data.level;
         name = data.name;
-            exp = data.exp;
+        exp = data.exp;
+        rupees = data.rupees;
+        data.beetleBucks = beetleBucks;
 
    //         Legs = data.Legs;
             
@@ -102,7 +125,15 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentStats == null)
+        {
 
+            GameObject head = GameObject.FindGameObjectWithTag("Header");
+            currentStats = Instantiate(gameStats,head.transform) as GameObject;
+            currentStats.transform.SetParent(head.transform);
+            
+        }
+       
     }
 }
 
@@ -112,6 +143,8 @@ class PlayerData //This class exists to hold the data and then be saved
     public int level;
     public string name;
     public int exp;
+    public int rupees;
+    public int beetleBucks;
     public List<GameObject> Legs = new List<GameObject>();
 
 }
